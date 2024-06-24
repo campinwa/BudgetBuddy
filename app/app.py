5c3d8dc3b025cba298b8a230daadbd4a5e715c57
campinwa <campin2156@gmail.com>
2024-06-24 15:55:12 +0700

    latest

---
+++ app/app.py
@@ -0,0 +1,119 @@
+from flask import Flask, request, jsonify, render_template
+from flask_mysqldb import MySQL
+import os
+from datetime import datetime, timedelta
+from apscheduler.schedulers.background import BackgroundScheduler
+
+# Assuming you are storing these in-memory for simplicity
+total_income = 0
+total_expense = 0
+
+
+app = Flask(__name__, template_folder='templates')
+
+# MySQL configurations
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'db')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'root_password')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'income_expense_db')
+
+mysql = MySQL(app)
+
+def create_transactions_table():
+    cur = mysql.connection.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS transactions (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            type VARCHAR(10) NOT NULL,
+            amount INT NOT NULL,
+            date DATETIME NOT NULL,
+            balance INT NOT NULL 
+        )
+    """)
+    mysql.connection.commit()
+    cur.close()
+
+with app.app_context():
+    create_transactions_table()
+
+def reset_global_state():
+    global total_income, total_expense
+    total_income = 0
+    total_expense = 0
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/add_income', methods=['POST'])
+def add_income():
+    amount = request.json['amount']
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT balance FROM transactions ORDER BY date DESC LIMIT 1")
+    last_balance = cur.fetchone()
+    if last_balance:
+        new_balance = last_balance[0] + amount
+    else:
+        new_balance = amount
+    cur.execute("INSERT INTO transactions (type, amount, date, balance) VALUES (%s, %s, %s, %s)", ('income', amount, datetime.now(), new_balance))
+    mysql.connection.commit()
+    cur.close()
+    return jsonify({'message': 'Income added successfully'})
+
+@app.route('/add_expense', methods=['POST'])
+def add_expense():
+    amount = request.json['amount']
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT balance FROM transactions ORDER BY date DESC LIMIT 1")
+    last_balance = cur.fetchone()
+    if last_balance:
+        new_balance = last_balance[0] - amount
+    else:
+        new_balance = -amount
+    cur.execute("INSERT INTO transactions (type, amount, date, balance) VALUES (%s, %s, %s, %s)", ('expense', amount, datetime.now(), new_balance))
+    mysql.connection.commit()
+    cur.close()
+    return jsonify({'message': 'Expense added successfully'})
+
+@app.route('/daily_summaries', methods=['GET'])
+def daily_summaries():
+    cur = mysql.connection.cursor()
+    cur.execute("""
+        SELECT 
+            date, 
+            SUM(CASE WHEN type='income' THEN amount ELSE 0 END) as total_income, 
+            SUM(CASE WHEN type='expense' THEN amount ELSE 0 END) as total_expense, 
+            MAX(balance) as balance
+        FROM transactions 
+        GROUP BY date 
+        ORDER BY date DESC
+    """)
+    data = cur.fetchall()
+    cur.close()
+
+    daily_summaries = []
+    for row in data:
+        daily_summaries.append({
+            'date': row[0].strftime("%Y-%m-%d %H:%M:%S"),
+            'total_income': row[1],
+            'total_expense': row[2],
+            'balance': row[3]
+        })
+
+    return jsonify(daily_summaries)
+
+@app.route('/delete_all', methods=['POST'])
+def delete_all_summaries():
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute("DELETE FROM transactions")
+        mysql.connection.commit()
+        cur.close()
+        return jsonify({'message': 'All daily summaries deleted successfully'})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ main.tf
@@ -0,0 +1,55 @@
+provider "google" {
+  project = "budgetbuddy" # Replace with your GCP project ID
+  region  = "us-west2"       # GCP region
+}
+
+resource "google_compute_instance" "app" {
+  name         = "budget-buddy-app"
+  machine_type = "e2-micro"
+  zone         = "us-west2-a" # Replace with a valid zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11" # Replace with a valid image if needed
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  tags = ["http-server"]
+
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y docker.io
+    sudo systemctl start docker
+    USERNAME=$(whoami)
+    sudo usermod -aG docker $USERNAME
+    sudo -u $USERNAME docker run -d -p 80:5000 campinwaladsae/income-expense-app
+  EOF
+}
+
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+   allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+
+  target_tags = ["http-server"]
+}
